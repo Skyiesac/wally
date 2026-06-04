5c47670af80b7c5ea9e0008c5b773763c2091d5c
Sachi Jain <jainsachi1202@gmail.com>
2026-06-04 23:54:14 +0530

    Dockerised the application and celery fixed.

--- backend/app/tasks/celery_app.py
+++ backend/app/tasks/celery_app.py
@@ -3,7 +3,7 @@ from celery import Celery
 from app.config import settings
 
 celery_app = Celery(
-    "name_builds",
+    "wally_builds",
     broker=settings.CELERY_BROKER_URL,
     backend=settings.CELERY_RESULT_BACKEND,
 )
